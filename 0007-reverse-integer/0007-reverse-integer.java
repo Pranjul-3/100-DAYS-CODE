3cfe5b7e55136fa03b10b30f296588708a6bef3e
Pranjul Dwivedi <114190023+Pranjul-3@users.noreply.github.com>
2023-10-14 23:30:10 +0530

    Time: 1 ms (97.65%), Space: 39.4 MB (70.44%) - LeetHub

--- 0007-reverse-integer/0007-reverse-integer.java
+++ 0007-reverse-integer/0007-reverse-integer.java
@@ -1,15 +1,18 @@
 class Solution {
     public int reverse(int x) {
-        int reverse = 0;
-        while (x != 0) {
-            if (reverse>2147483647/10  || reverse<-2147483648/10)
-            {
-                return 0;
-            }
-            int remainder = x % 10;
-            reverse = reverse * 10 + remainder;
-            x = x / 10;
+        
+        long reversedx=0;
+        int remainder=0;
+
+        int temp = x;
+
+        while (temp != 0) {
+            remainder = temp % 10;
+            reversedx = (reversedx * 10) + remainder;
+            temp /= 10;
         }
-        return reverse;
+        if(reversedx > Integer.MAX_VALUE || reversedx < Integer.MIN_VALUE)
+            return 0;
+        return (int)reversedx;
     }
 }
