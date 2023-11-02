29603e135337f6f9f7e227f9cf1fcf968c34f80f
Pranjul Dwivedi <114190023+Pranjul-3@users.noreply.github.com>
2023-11-02 22:23:23 +0530

    Time: 8 ms (98.41%), Space: 44.1 MB (56.51%) - LeetHub

--- 0455-assign-cookies/0455-assign-cookies.java
+++ 0455-assign-cookies/0455-assign-cookies.java
@@ -1,17 +1,24 @@
+import java.util.Arrays;
+
 class Solution {
     public int findContentChildren(int[] g, int[] s) {
         Arrays.sort(g);
         Arrays.sort(s);
-        int c=0,i=0,j=0;
-        while(i<g.length && j<s.length)
-        {
-            if(s[j] >= g[i])
-            {
-               c++;
-               i++;
+        int c = 0;
+        int j = 0;
+        int l = g.length;
+        int l1 = s.length;
+
+        for (int i = 0; i < l && j < l1; i++) {
+            while (j < l1 && s[j] < g[i]) {
+                j++;
             }
-            j++;
+            if (j < l1) {
+                c++;
+                j++;
+            }
         }
-     return i;   
+
+        return c;
     }
 }
