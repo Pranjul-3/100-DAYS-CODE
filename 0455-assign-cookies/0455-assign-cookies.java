fac9b2be2365784a543213c268d6746a00b03e1d
Pranjul Dwivedi <114190023+Pranjul-3@users.noreply.github.com>
2023-11-04 17:22:12 +0530

    Time: 9 ms (36.56%), Space: 44.2 MB (55.68%) - LeetHub

--- 0455-assign-cookies/0455-assign-cookies.java
+++ 0455-assign-cookies/0455-assign-cookies.java
@@ -15,3 +15,21 @@ class Solution {
      return i;   
     }
 }
+
+/** class Solution {
+    public int findContentChildren(int[] g, int[] s) {
+        int c = 0;
+        Arrays.sort(g);
+        Arrays.sort(s);
+        for (int i = 0; i < g.length; i++) {
+            for (int j = 0; j < s.length; j++) {
+                if (s[j] >= g[i]) {
+                    c++;
+                    s[j] = -1;
+                    break;
+                }
+            }
+        }
+        return c;
+    }
+} **/
