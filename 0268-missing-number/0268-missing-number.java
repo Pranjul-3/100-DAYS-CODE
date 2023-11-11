f0738d965f9f17b76604303f7ae4aadccd685311
Pranjul Dwivedi <114190023+Pranjul-3@users.noreply.github.com>
2023-11-11 20:17:52 +0530

    Time: 0 ms (100%), Space: 44.1 MB (36.09%) - LeetHub

--- 0268-missing-number/0268-missing-number.java
+++ 0268-missing-number/0268-missing-number.java
@@ -1,5 +1,22 @@
 class Solution {
     public int missingNumber(int[] nums) {
+        int n = nums.length;
+       int totalSum = n*(n+1)/2;
+        int sum = 0;
+        for(int i =0;i<n;i++){
+            sum = sum+nums[i]; 
+        }
+        return totalSum - sum;
+    }
+}
+
+
+
+
+
+
+/** class Solution {
+    public int missingNumber(int[] nums) {
         int n=nums.length;
         Arrays.sort(nums);
         for(int i=0; i<n; i++)
@@ -11,4 +28,4 @@ class Solution {
         }
         return n;
     }
-}
+} **/
