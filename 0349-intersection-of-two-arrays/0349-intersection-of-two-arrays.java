83c77666c239dda9bee82c0b40a5e2460f15b5a6
Pranjul Dwivedi <114190023+Pranjul-3@users.noreply.github.com>
2023-11-14 23:29:21 +0530

    Time: 3 ms (47.62%), Space: 41.6 MB (99.03%) - LeetHub

--- 0349-intersection-of-two-arrays/0349-intersection-of-two-arrays.java
+++ 0349-intersection-of-two-arrays/0349-intersection-of-two-arrays.java
@@ -3,53 +3,55 @@ class Solution {
         Arrays.sort(nums1);
         Arrays.sort(nums2);
 
-        List<Integer> result = new ArrayList<>();
+        List<Integer> res = new ArrayList<>();
 
-        int i = 0; // Pointer for nums1
-        int j = 0; // Pointer for nums2
+        int i = 0; 
+        int j = 0; 
 
         while (i < nums1.length && j < nums2.length) {
             if (nums1[i] == nums2[j]) {
-                // Found an intersection
-                result.add(nums1[i]);
-
-                // Move both pointers to avoid duplicate entries
+                
+                res.add(nums1[i]);
                 i++;
                 j++;
-            } else if (nums1[i] < nums2[j]) {
-                // Move the pointer for nums1
+            } 
+            else if (nums1[i] < nums2[j])
+            {
                 i++;
-            } else {
-                // Move the pointer for nums2
+            } 
+            else 
+            {
+                
                 j++;
             }
         }
 
-        // Convert the result list to an array
-        int[] resultArray = new int[result.size()];
-        for (int k = 0; k < result.size(); k++) {
-            resultArray[k] = result.get(k);
+       
+        int[] a = new int[res.size()];
+        for (int k = 0; k < res.size(); k++) {
+            a[k] = res.get(k);
         }
 
-        // Remove duplicates in the resultArray
+        
         int c = 1;
-        Arrays.sort(resultArray);
-        for (int k = 1; k < resultArray.length; k++) {
-            if (resultArray[k] != resultArray[k - 1]) {
-                resultArray[c] = resultArray[k];
+        Arrays.sort(a);
+        for (int k = 1; k < a.length; k++) {
+            if (a[k] != a[k - 1]) {
+            a[c] = a[k];
                 c++;
             }
         }
 
-        // Check if resultArray is empty
-        if (resultArray.length == 0) {
+        
+        if (a.length == 0) 
+        {
             return new int[]{};
         }
 
-        // Create a new array with unique elements
-        int[] uniqueArray = Arrays.copyOf(resultArray, c);
+        
+        int[] b = Arrays.copyOf(a, c);
 
-        return uniqueArray;
+        return b;
     }
 }
     
