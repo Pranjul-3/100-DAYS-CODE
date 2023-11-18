7937df8e2a774889b0894b14bd62d67365ea0f1e
Pranjul Dwivedi <114190023+Pranjul-3@users.noreply.github.com>
2023-11-18 21:49:17 +0530

    Time: 0 ms (100.00%), Space: 40.6 MB (14.42%) - LeetHub

--- 0434-number-of-segments-in-a-string/0434-number-of-segments-in-a-string.java
+++ 0434-number-of-segments-in-a-string/0434-number-of-segments-in-a-string.java
@@ -17,3 +17,21 @@ class Solution {
 
 
 
+
+
+/** class Solution {
+    public int countSegments(String s) {
+        s=s.trim();
+        if(s.isEmpty())
+        {
+            return 0;
+        }
+        String str [] =s.split("\\s+");
+        return str.length;
+    }
+} **/
+
+
+
+
+
