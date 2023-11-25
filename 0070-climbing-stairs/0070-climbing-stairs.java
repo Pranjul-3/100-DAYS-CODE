e3bb40abbbf40f038fdb529d21fe7e5e8562e88f
Pranjul Dwivedi <114190023+Pranjul-3@users.noreply.github.com>
2023-11-25 23:20:08 +0530

    Time: 0 ms (100.00%), Space: 39.4 MB (28.60%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.java
+++ 0070-climbing-stairs/0070-climbing-stairs.java
@@ -1,8 +1,8 @@
 class Solution {
     public int climbStairs(int n) {
         if (n < 3) return n;
-            int first = 1;
-            int second =2;
+            int first=1;
+            int second=2;
 
         for (int i = 3; i<=n; i++)
         {   int temp = first+second;
