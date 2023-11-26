cbb26980e902853f1c052519dac2d85d3cc29f07
Pranjul Dwivedi <114190023+Pranjul-3@users.noreply.github.com>
2023-11-26 11:03:18 +0530

    Time: 0 ms (100.00%), Space: 40.6 MB (34.26%) - LeetHub

--- 0917-reverse-only-letters/0917-reverse-only-letters.java
+++ 0917-reverse-only-letters/0917-reverse-only-letters.java
@@ -1,5 +1,31 @@
 class Solution {
     public String reverseOnlyLetters(String s) {
+        char[] arr = s.toCharArray();
+        int i = 0 , j = s.length()-1;
+
+        while (i < j)
+        {
+            char c1 = arr[i];
+            char c2 = arr[j];
+            if (Character.isLetter(c1) && Character.isLetter(c2)) {
+                arr[i++] = c2;
+                arr[j--] = c1;
+            }
+            if(!Character.isLetter(c2)){
+                j--;
+            }
+            if (!Character.isLetter(c1)){
+                i++;
+            }
+        }
+
+        return new String(arr);
+
+    }
+}
+
+/**class Solution {
+    public String reverseOnlyLetters(String s) {
         String str1="";
         String str2="";
         for(int i=0; i<s.length(); i++)
@@ -24,4 +50,4 @@ class Solution {
         }
         return str2;
     }
-}
+}**/
