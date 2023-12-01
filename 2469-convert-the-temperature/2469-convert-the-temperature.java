fbba6628125d0577e97e49f3b22b5e0fbf6493b9
Pranjul Dwivedi <114190023+Pranjul-3@users.noreply.github.com>
2023-12-01 18:30:00 +0530

    Time: 0 ms (100.00%), Space: 40.8 MB (36.94%) - LeetHub

--- 2469-convert-the-temperature/2469-convert-the-temperature.java
+++ 2469-convert-the-temperature/2469-convert-the-temperature.java
@@ -1,11 +1,22 @@
-class Solution {
+class Solution
+{
+    public double[] convertTemperature(double celsius) 
+    {
+        return new double[] {celsius + 273.15,(celsius * 1.80) + 32.00};
+          
+    }
+}
+
+
+
+/**class Solution {
     public double[] convertTemperature(double celsius) {
      
         double temp [] = {celsius + 273.15,(celsius * 1.80) + 32.00};
           
         return temp;
     }
-}
+}**/
 
 
 
