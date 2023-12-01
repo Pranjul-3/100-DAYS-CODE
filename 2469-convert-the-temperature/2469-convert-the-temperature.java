4781ac3428046105d2e0019db35eb3167a216174
Pranjul Dwivedi <114190023+Pranjul-3@users.noreply.github.com>
2023-12-01 18:30:48 +0530

    Time: 0 ms (100.00%), Space: 40.9 MB (36.94%) - LeetHub

--- 2469-convert-the-temperature/2469-convert-the-temperature.java
+++ 2469-convert-the-temperature/2469-convert-the-temperature.java
@@ -1,11 +1,21 @@
-class Solution
+class Solution {
+    public double[] convertTemperature(double celsius) {
+     double kelvin = celsius + 273.15;
+        double fahrenheit = (celsius * 1.80) + 32.00;
+        double temp [] = {kelvin , fahrenheit};
+          
+        return temp;
+    }
+}
+
+/**class Solution
 {
     public double[] convertTemperature(double celsius) 
     {
         return new double[] {celsius + 273.15,(celsius * 1.80) + 32.00};
           
     }
-}
+}**/
 
 
 
@@ -22,12 +32,3 @@ class Solution
 
 
 
-/**class Solution {
-    public double[] convertTemperature(double celsius) {
-     double kelvin = celsius + 273.15;
-        double fahrenheit = (celsius * 1.80) + 32.00;
-        double temp [] = {kelvin , fahrenheit};
-          
-        return temp;
-    }
-}**/
