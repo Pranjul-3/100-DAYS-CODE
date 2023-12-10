3a81f8ea84ae06919ae2501fb8ce124f58a8a882
Pranjul Dwivedi <114190023+Pranjul-3@users.noreply.github.com>
2023-12-10 21:26:58 +0530

    Time: 0 ms (100.00%), Space: 40.6 MB (43.69%) - LeetHub

--- 0877-stone-game/0877-stone-game.java
+++ 0877-stone-game/0877-stone-game.java
@@ -1,5 +1,40 @@
 class Solution {
     public boolean stoneGame(int[] piles) {
-        return true;
+        int a=0;
+        for(int i=0; i<piles.length; i=i+2){
+            a=a+piles[i];
+        }
+        int b=0;
+        for(int i=1; i<piles.length; i=i+2){
+            b=b+piles[i];
+        }
+        int a1=0;
+        for(int i=piles.length-1; i>=0 ; i=i-2){
+            a1=a1+piles[i];
+        }
+        int b1=0;
+        for(int i=piles.length-2; i>=0 ; i=i-2){
+            b1=b1+piles[i];
+        } 
+   boolean res = (a>b || a1>b1); 
+        return res;
+  
     }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+/** class Solution {
+    public boolean stoneGame(int[] piles) {
+        return true;
+    }
+} **/
