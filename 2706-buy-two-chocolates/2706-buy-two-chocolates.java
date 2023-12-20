f4c4b4467583878c5e890240d1dc7942e868f668
Pranjul Dwivedi <114190023+Pranjul-3@users.noreply.github.com>
2023-12-20 19:10:29 +0530

    Time: 2 ms (77.87%), Space: 44.5 MB (5.53%) - LeetHub

---
+++ 2706-buy-two-chocolates/2706-buy-two-chocolates.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int buyChoco(int[] prices, int money) {
+        Arrays.sort(prices);
+        int sum=0;
+        for(int i=0;i<2;i++)
+        {
+            sum=sum+prices[i];
+        }
+        if(money-sum>=0)
+        {
+          return money-sum;
+        }
+        return money;
+    }
+}
