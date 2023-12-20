9530eae092a4e547cad449f7351e32765995221a
Pranjul Dwivedi <114190023+Pranjul-3@users.noreply.github.com>
2023-12-20 13:54:41 +0530

    Time: 1 ms (67.26%), Space: 44.1 MB (5.63%) - LeetHub

--- 2455-average-value-of-even-numbers-that-are-divisible-by-three/2455-average-value-of-even-numbers-that-are-divisible-by-three.java
+++ 2455-average-value-of-even-numbers-that-are-divisible-by-three/2455-average-value-of-even-numbers-that-are-divisible-by-three.java
@@ -9,12 +9,12 @@ class Solution {
             }
         }
 
-        // Check if there are elements that satisfy the condition
+        
         if (c != 0) {
             return s / c;
         } else {
-            // Handle the case when there are no elements that satisfy the condition
-            return 0; // You can choose to return 0 or another value based on your requirements
+            
+            return 0; 
         }
     }
 }
