c38cdaf686068ff46e5e8c42d931d7ad724aab08
Pranjul Dwivedi <114190023+Pranjul-3@users.noreply.github.com>
2023-12-21 19:14:20 +0530

    Time: 2 ms (79.00%), Space: 44.6 MB (5.58%) - LeetHub

--- 2706-buy-two-chocolates/2706-buy-two-chocolates.java
+++ 2706-buy-two-chocolates/2706-buy-two-chocolates.java
@@ -13,3 +13,15 @@ class Solution {
         return money;
     }
 }
+
+
+
+
+
+
+/**class Solution {
+    public int buyChoco(int[] prices, int money) {
+        Arrays.sort(prices);
+        return prices[0] + prices[1] <= money?money - (prices[0] + prices[1]):money;
+    }
+}**/
