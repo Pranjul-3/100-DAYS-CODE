186a3033b937c32cba180be41b2bf6954a92d1c3
Pranjul Dwivedi <114190023+Pranjul-3@users.noreply.github.com>
2023-12-23 19:27:53 +0530

    Time: 0 ms (100.00%), Space: 40.5 MB (5.43%) - LeetHub

--- 0172-factorial-trailing-zeroes/0172-factorial-trailing-zeroes.java
+++ 0172-factorial-trailing-zeroes/0172-factorial-trailing-zeroes.java
@@ -1,12 +1,12 @@
 class Solution {
     public int trailingZeroes(int n) {
-        int count = 0;
-
-        // Count the number of factors of 5 in the range [1, n]
-        for (int i = 5; n / i > 0; i *= 5) {
-            count += n / i;
-        }
-
-        return count;
+        int ans=0;
+       while(n>=5)
+       {
+           n=n/5;
+           ans=ans+n;
+       }
+        
+        return ans;
     }
 }
