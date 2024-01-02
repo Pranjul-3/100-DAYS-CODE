905a176528cb04e8b4a13017455779fec99b2b3b
Pranjul Dwivedi <114190023+Pranjul-3@users.noreply.github.com>
2024-01-02 19:21:22 +0530

    Time: 2 ms (98.46%), Space: 45 MB (6.97%) - LeetHub

--- 2535-difference-between-element-sum-and-digit-sum-of-an-array/2535-difference-between-element-sum-and-digit-sum-of-an-array.java
+++ 2535-difference-between-element-sum-and-digit-sum-of-an-array/2535-difference-between-element-sum-and-digit-sum-of-an-array.java
@@ -4,18 +4,11 @@ class Solution {
         for(int a: nums)
         {
             s1=s1+a;
-            if(a>9)
-            {
                 while(a!=0)
                 {
                 int r=a%10;
                 s2=s2+r;
                 a=a/10;
-            }
-        }
-            else
-            {
-                s2=s2+a;
             }
         
     }
