1bad99a1c26c36708112a65782ad411b44f305c0
Pranjul Dwivedi <114190023+Pranjul-3@users.noreply.github.com>
2024-01-08 21:10:12 +0530

    Time: 0 ms (100.00%), Space: 40.1 MB (31.47%) - LeetHub

---
+++ 0461-hamming-distance/0461-hamming-distance.java
@@ -0,0 +1,31 @@
+class Solution {
+    public int hammingDistance(int x, int y) {
+        int count = 0;
+        int a = x^y;
+        String b = Integer.toBinaryString(a);
+        for(int i =0; i<b.length(); i++){
+            if(b.charAt(i)=='1')count++;
+        }
+        return count;
+        
+    }
+}
+
+
+/**class Solution {
+    public int hammingDistance(int x, int y) {
+        return Integer.bitCount(x^y);
+    }
+}
+class Solution {
+    public int hammingDistance(int x, int y) {
+        int count=0;
+        while(x>0||y>0)
+        {
+            if(x%2!=y%2)count++;
+            x/=2;
+            y/=2;
+        }
+        return count;
+    }
+}**/
