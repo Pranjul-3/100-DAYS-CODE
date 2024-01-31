8dddf17c044a85a9125b9ef5db17fb67ab630272
Pranjul Dwivedi <114190023+Pranjul-3@users.noreply.github.com>
2024-01-31 21:26:36 +0530

    Time: 33 ms (73.83%), Space: 77 MB (5.23%) - LeetHub

---
+++ 0456-132-pattern/0456-132-pattern.java
@@ -0,0 +1,32 @@
+class Solution {
+    public boolean find132pattern(int[] nums) {
+        if (nums == null || nums.length < 3)
+            return false;
+        
+        int n = nums.length;
+        int[] minLeft = new int[n];
+        minLeft[0] = nums[0];
+        
+        for (int i = 1; i < n; i++) {
+            minLeft[i] = Math.min(minLeft[i - 1], nums[i]);
+        }
+        
+        Stack<Integer> stack = new Stack<>();
+        
+        for (int j = n - 1; j >= 0; j--) {
+            if (nums[j] > minLeft[j]) {
+                while (!stack.isEmpty() && stack.peek() <= minLeft[j]) {
+                    stack.pop();
+                }
+                
+                if (!stack.isEmpty() && stack.peek() < nums[j]) {
+                    return true;
+                }
+                
+                stack.push(nums[j]);
+            }
+        }
+        
+        return false;
+    }
+    }
